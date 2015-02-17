05579fde23899c731c4bcc5b32f18635fe64654a
Mats Loock <mats.loock@lnu.se>
2015-02-17 14:30:31 +0100

    First commit.

--- 1-vandalen/strings/script.js
+++ 1-vandalen/strings/script.js
@@ -4,16 +4,25 @@ window.onload = function(){
 
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var convertString = function(str){
-		// Plats för förändring.		
-		// Returnera den konverterade strängen.
-		// Vid fel, kasta ett undantag med ett meddelande till användaren. 
-	
+		// Ersätt alla a och A med #.
+		var result = str.replace(/a/gi, "#");
 
+		// Konvertera strängen till array med tecken och växla
+		// mellan gemener och versaler, och tvärtom, om det
+		// är en bokstav.
+		result = result.split("");
+		for (var i = result.length - 1; i >= 0; i = i - 1)
+		{
+			if (result[i] === result[i].toLocaleUpperCase()){
+				result[i] = result[i].toLocaleLowerCase();
+			}
+			else if (result[i] === result[i].toLocaleLowerCase()){
+				result[i] = result[i].toLocaleUpperCase();
+			}
+		}
 
-
-
-
-
+		// Konvertera arrayen med tecken till en sträng.
+		return result.join("");
 	};
 	// ------------------------------------------------------------------------------
 
@@ -30,7 +39,7 @@ window.onload = function(){
 		p.classList.remove( "error");
 
 		try {
-			var answer = convertString(input.value) // Läser in texten från textrutan och skickar till funktionen "convertString"
+			var answer = convertString(input.value); // Läser in texten från textrutan och skickar till funktionen "convertString"
 			p.innerHTML = answer;		// Skriver ut texten från arrayen som skapats i funktionen.	
 		} catch (error){
 			p.classList.add( "error"); // Växla CSS-klass, IE10+
