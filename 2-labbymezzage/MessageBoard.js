9fc5a83e78b7715395606e9f0896a934d6ed4ce4
Mats Loock <mats.loock@lnu.se>
2015-02-18 12:18:52 +0100

    Finished step 6.

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -1,5 +1,42 @@
 "use strict";
 
 var MessageBoard = {
+    messages: [],
 
+    addMessage: function(message) {
+        MessageBoard.messages.push(message);
+    },
+
+    refreshMessageCounter: function(container) {
+        container.innerHTML = MessageBoard.messages.length.toString();
+    },
+
+    renderLastMessage: function(container) {
+        MessageBoard.renderMessage(container, MessageBoard.messages.slice(-1)[0]);
+    },
+
+    renderMessage: function(container, message) {
+        // Text.
+        var article = document.createElement("article");
+        article.innerHTML = message.toHtmlString();
+        container.appendChild(article);
+
+        // Tid.
+        var div = document.createElement("div");
+        div.innerHTML = message.toShortTimeString();
+        article.appendChild(div);
+    },
+
+    renderMessages: function(container) {
+        // Ta bort alla article-element i containern.
+        var articles = container.querySelectorAll("article");
+        for (var i = 0; i < articles.length; i = i + 1) {
+            container.removeChild(articles[i]);
+        }
+
+        // Lägger till alla meddelanden till containern.
+        MessageBoard.messages.forEach(function(m) {
+            MessageBoard.renderMessage(container, m);
+        });
+    }
 };
--- 2-labbymezzage/app.js
+++ 2-labbymezzage/app.js
@@ -1,9 +1,23 @@
 "use strict";
 
 window.onload = function() {
-    var message = new Message("Hej hopp!", new Date());
-    alert(message);
-    alert(message.toTextHtmlString());
-    message.setText("Hej då!");
-    alert(message);
+    //MessageBoard.messages.push(new Message("#1", new Date()));
+    //MessageBoard.messages.push(new Message("#2", new Date()));
+    //MessageBoard.messages.push(new Message("#3", new Date()));
+    //MessageBoard.messages.push(new Message("#4", new Date()));
+    //
+    //alert(MessageBoard.messages[0].getDate().toISOString().slice(11, 16));
+
+    var messageText = document.querySelector("textarea");
+    var messageContainer = document.querySelector("section");
+    var submit = document.querySelector("input[type=submit]");
+    var counter = document.querySelector("section header span");
+
+    submit.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        MessageBoard.addMessage(new Message(messageText.value, new Date()));
+        MessageBoard.renderLastMessage(messageContainer);
+        MessageBoard.refreshMessageCounter(counter);
+    });
 };
